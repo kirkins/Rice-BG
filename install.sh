b6935a1c8f48d71110de2283c2986d00ce183c63
Philip Kirkbride <kirkins@gmail.com>
2016-10-24 11:14:03 -0400

    update error messages when feh or npm not found

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 # Check if feh is installed
 hash feh &> /dev/null
 if [ $? -eq 1 ]; then
-  echo >&2 "feh not found."
+  echo >&2 "feh not found. Please google how to install feh on your OS."
   exit
 fi
 
 # Check if npm is installed
 hash npm &> /dev/null
 if [ $? -eq 1 ]; then
-  echo >&2 "npm not found."
+  echo >&2 "npm not found. Install npm and try again."
 else
   # install required node packages
   npm install -g phantomjs browserify
