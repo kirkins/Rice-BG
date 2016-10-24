6511a77132db74b872102eecab53a5ec5ee58ef0
Philip Kirkbride <kirkins@gmail.com>
2016-10-24 11:13:01 -0400

    add comments

--- makeHome.sh
+++ makeHome.sh
@@ -1,9 +1,13 @@
 # Create JSON file with system info in json
 echo '{"user": "'$USER'","ostype": "'$OSTYPE'"}' > content/system.json 
+# Bundle javascript, so we can use some node.js syntax
 browserify content/js/scripts.js -o content/js/bundle.js
+# Get the screen size
 Xaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f1)
 Yaxis=$(xrandr --current | grep '*' | uniq | awk '{print $1}' | cut -d 'x' -f2)
 WinSize=$Xaxis"px*"$Yaxis"px"
+# Convert the html/js into a png image.
 phantomjs scripts/rasterize.js content/index.html output/home.png $WinSize
+# Set the image as background using feh
 feh --bg-fill output/home.png
 
