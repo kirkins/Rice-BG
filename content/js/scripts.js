1ad8991e0733bfa3c70fe644f3bd410a6e6221c1
Philip Kirkbride <kirkins@gmail.com>
2016-10-24 11:04:22 -0400

    add comments in regards to browserify bundling

--- content/js/scripts.js
+++ content/js/scripts.js
@@ -1,5 +1,9 @@
+// This script uses require and thus must be compiled using browserify.js
+// If you try to run this script directly, it won't work. This is compiled into
+// bundle.js in this same directory
 var system = require('../system.json');
-console.log(system);
-console.log(system.user);
-document.body.innerHTML += '<div class="user">'+system.user+'</div>';
-document.body.innerHTML += '<div class="ostype">'+system.ostype+'</div>';
+var html = '<div class="bottom-right">';
+html += '<div class="user">'+system.user+'</div>';
+html += '<div class="ostype">'+system.ostype+'</div>';
+html += '</div>';
+document.body.innerHTML += html;
